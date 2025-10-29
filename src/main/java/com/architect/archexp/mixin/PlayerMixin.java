5a51ddb5fd461aacfc5be958d59cb44e29eca347
Razz <bllopblep@gmail.com>
2025-10-28 21:36:10 -0400

    wip market, adjust amulets, comment out debugging

--- src/main/java/com/architect/archexp/mixin/PlayerMixin.java
+++ src/main/java/com/architect/archexp/mixin/PlayerMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
