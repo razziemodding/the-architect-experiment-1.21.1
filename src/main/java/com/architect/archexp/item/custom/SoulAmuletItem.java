257fbbd8e02214e382db8194bba16b2d97a0bf71
Razz <bllopblep@gmail.com>
2025-11-25 23:20:29 -0500

    soul rework: redo armor un-rendering strategy

--- src/main/java/com/architect/archexp/item/custom/SoulAmuletItem.java
+++ src/main/java/com/architect/archexp/item/custom/SoulAmuletItem.java
@@ -3,8 +3,6 @@ package com.architect.archexp.item.custom;
 import com.architect.archexp.TheArchitectExperiment;
 import com.architect.archexp.util.ModComponents;
 import com.architect.archexp.sound.ModSounds;
-import com.mojang.datafixers.util.Pair;
-import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -12,9 +10,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.network.packet.s2c.play.EntityEquipmentUpdateS2CPacket;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Hand;
@@ -22,7 +18,6 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import java.util.List;
 import java.util.Objects;
 
 public class SoulAmuletItem extends Item { //todo: fix effect not clearing if they shift click into an inventory
@@ -36,7 +31,6 @@ public class SoulAmuletItem extends Item { //todo: fix effect not clearing if th
             return TypedActionResult.fail(this.getDefaultStack());
         }
         ItemStack handItem = user.getEquippedStack(EquipmentSlot.OFFHAND);
-        ItemStack emp = new ItemStack(Items.AIR);
 
         if (!handItem.get(ModComponents.SOUL_AMULET_PLR).equals("") && !handItem.get(ModComponents.SOUL_AMULET_PLR).equals(user.getUuidAsString())) {
             TheArchitectExperiment.clearSoulComponents(handItem);
@@ -46,23 +40,8 @@ public class SoulAmuletItem extends Item { //todo: fix effect not clearing if th
 
         if (Objects.equals(handItem.get(ModComponents.SOUL_AMULET_ACTIVE), true)) {
             //TheArchitectExperiment.LOGGER.info("active");
-
             TheArchitectExperiment.removeSoulEffects(user, handItem);
-
-            if (!world.isClient) {
-                for (ServerPlayerEntity tracking : PlayerLookup.tracking(user)) {
-                    //TheArchitectExperiment.LOGGER.info("la");
-                    if (tracking != user) {
-                        tracking.networkHandler.sendPacket(
-                                new EntityEquipmentUpdateS2CPacket(user.getId(),
-                                        List.of(Pair.of(EquipmentSlot.HEAD, user.getEquippedStack(EquipmentSlot.HEAD)),
-                                                Pair.of(EquipmentSlot.CHEST, user.getEquippedStack(EquipmentSlot.CHEST)),
-                                                Pair.of(EquipmentSlot.LEGS, user.getEquippedStack(EquipmentSlot.LEGS)),
-                                                Pair.of(EquipmentSlot.FEET, user.getEquippedStack(EquipmentSlot.FEET)))));
-                    }
-                }
-            }
-            user.getItemCooldownManager().set(handItem.getItem(), 360); //45-second cooldown
+            user.getItemCooldownManager().set(handItem.getItem(), 20); //45-second cooldown
             return TypedActionResult.success(handItem);
         } else {
             //TheArchitectExperiment.LOGGER.info("false");
@@ -73,18 +52,6 @@ public class SoulAmuletItem extends Item { //todo: fix effect not clearing if th
 
                 user.setVelocity(vel.x, vel.y, vel.z);
                 user.velocityModified = true;
-
-                for (ServerPlayerEntity tracking : PlayerLookup.tracking(user)) {
-                    //TheArchitectExperiment.LOGGER.info("la");
-                    if (tracking != user) {
-                        tracking.networkHandler.sendPacket(
-                                new EntityEquipmentUpdateS2CPacket(user.getId(),
-                                        List.of(Pair.of(EquipmentSlot.HEAD, emp),
-                                                Pair.of(EquipmentSlot.CHEST, emp),
-                                                Pair.of(EquipmentSlot.LEGS, emp),
-                                                Pair.of(EquipmentSlot.FEET, emp))));
-                    }
-                }
             }
         }
 
