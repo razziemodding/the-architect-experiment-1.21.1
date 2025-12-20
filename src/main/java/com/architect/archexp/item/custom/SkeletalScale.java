d1a8374b6a5f17433e6b449592ecc4aa87edae04
Razz <bllopblep@gmail.com>
2025-12-20 15:21:12 -0500

    skeletal scale tweaks

--- src/main/java/com/architect/archexp/item/custom/SkeletalScale.java
+++ src/main/java/com/architect/archexp/item/custom/SkeletalScale.java
@@ -1,11 +1,16 @@
 package com.architect.archexp.item.custom;
 
+import com.architect.archexp.damage_source.ModDamageSources;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -21,7 +26,10 @@ public class SkeletalScale extends SwordItem {
 
         if (!user.getWorld().isClient) {
             CommandManager manager = user.getServer().getCommandManager();
-            manager.executeWithPrefix(user.getServer().getCommandSource(), "execute at " + entity.getNameForScoreboard() + " run summon minecraft:evoker_fangs ~ ~ ~");
+            manager.executeWithPrefix(user.getServer().getCommandSource(),
+                    "execute as " + entity.getNameForScoreboard() + " at " + entity.getNameForScoreboard() + " run pal animatedcircle minecraft:small_gust ~ ~1 ~ 100 2 0 3.14 true false true 2.0 0.0 0.0 0.0 2.0 1.0 true 0");
+            DamageSource source = new DamageSource(user.getWorld().getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(DamageTypes.MAGIC));
+            entity.damage(source, 4);
             user.getItemCooldownManager().set(this, 45);
             stack.damage((int) (stack.getMaxDamage() * 0.013), user, EquipmentSlot.MAINHAND);
             return ActionResult.SUCCESS;
